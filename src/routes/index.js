a5d20c37caf73715d84473d1add77053736d3251
Franky <frankychan007@gmail.com>
2021-05-26 16:36:53 -0400

    Init Express and Node server

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+router.get("/", (req, res, next) => {
+  res.status(200);
+  res.send({
+    message: "Welcome to Express API template",
+    count: parseInt(Math.random() * 100)
+  });
+});
+module.exports = router;
