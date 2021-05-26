2a150e26a623bf504209d6679bdd7bf04579bfbd
Franky <frankychan007@gmail.com>
2021-05-26 18:52:24 -0400

    Added prettier and eslint

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-import debug from "debug";
-import http from "http";
-import app from "../app";
+import debug from 'debug';
+import http from 'http';
+import app from '../app';
 
 // Normalize a port into a number, string, or false.
-const normalizePort = val => {
+const normalizePort = (val) => {
   const port = parseInt(val, 10);
   if (Number.isNaN(port)) {
     return val;
@@ -16,22 +16,22 @@ const normalizePort = val => {
   return false;
 };
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 const server = http.createServer(app);
 
-const onError = error => {
-  if (error.syscall !== "listen") {
+const onError = (error) => {
+  if (error.syscall !== 'listen') {
     throw error;
   }
-  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case "EACCES":
+    case 'EACCES':
       alert(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
-    case "EADDRINUSE":
+    case 'EADDRINUSE':
       alert(`${bind} is already in use`);
       process.exit(1);
       break;
@@ -42,11 +42,11 @@ const onError = error => {
 
 const onListening = () => {
   const addr = server.address();
-  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
   debug(`Listening on ${bind}`);
 };
 
 // Listen on provided port, and other network interfaces.
 server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
+
 const router = express.Router();
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.status(200);
   res.send({
-    message: "Welcome to Express API template",
-    count: parseInt(Math.random() * 100)
+    message: 'Welcome to Express API template',
+    count: parseInt(Math.random() * 100),
   });
 });
 module.exports = router;
